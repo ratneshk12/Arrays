9498c13c616f29cf885bf2c4a40f22ed37b65f96
Ratnesh Kumar <ratneshkumatr8699@gmail.com>
2026-02-04 17:52:00 +0530

    updated

--- SearchAnArray.java
+++ SearchAnArray.java
@@ -1,15 +1,23 @@
-public class Main{
+public class SearchAnArray{
     public static void main(String[] args) {
         
         int [] numbers = {10, 20, 30, 40, 50};
-        int target = 30;
+        int target = 50;
+
+        boolean isFound = false;
         
         for (int i = 0; i < numbers.length; i++) {
             if (target == numbers [i])
             {
                 System.out.println("Element found at index: " + i);
+                isFound = true;
                 break;
             }
+        }
+        if (isFound) {
+            System.out.println("Element found in the array.");
+        } else {
+            System.out.println("Element not found in the array.");
         }
     }
 }
